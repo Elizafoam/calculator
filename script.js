351fe582e05849506faaadcbc36d1748402529f1
Elizabeth <elizafoam@gmail.com>
2023-08-24 18:56:58 -0400

    create operator functions

---
+++ script.js
@@ -0,0 +1,33 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+function add(a, b) {
+  parseInt(a)
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(firstNumber, secondNumber, operator){
+  switch(operator){
+    case '+':
+      return add(firstNumber, secondNumber);
+    case '-':
+      return subtract(firstNumber, secondNumber);
+    case 'x':
+      return multiply(firstNumber, secondNumber);
+    case '÷':
+      return divide(firstNumber, secondNumber);
+  }
+}
